c33c4ed5741a69b627e4a293b2ef6b8b4d7d2e6d
unknown <inf136435@lab-45-3.cs.put.poznan.pl>
2019-11-04 16:11:18 +0100

    float b

--- Program.java
+++ Program.java
@@ -4,6 +4,7 @@ public class HelloWorld {
         // Prints "Hello, World" to the terminal window.
 	float a = 1;
         System.out.println("Hello, World!");
+	float b = 2;
 	System.out.println("Hello Sailor!");
     }
 
